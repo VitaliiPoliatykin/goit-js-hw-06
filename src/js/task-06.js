5c395d4d62bfdf4a9272689a617de086aa3a4b26
VitaliiPoliatykin <102904854+VitaliiPoliatykin@users.noreply.github.com>
2022-09-06 01:12:44 +0300

    Done 7-8
    Ошибка в 6м задании

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -1,2 +1,17 @@
-const input = document.querySelector(`#valiadation-input`);
-input.addEventListener(`blur`, `onBlur`)
+const refs = {
+    validationInput: document.querySelector(`#valiadation-input`),
+};
+
+const testValidation = (quantity) => {
+    if (quantity.currentTarget.value === "") return;
+
+    quantity.currentTarget.value.length > refs.validationInput.dataset.length
+        ? refs.validationInput.classList.add("invalid")
+        : refs.validationInput.classList.add("valid")
+}
+
+const resetValidation = (quantity) =>
+    refs.validationInput.classList.remove("valid", "invalid");
+
+refs.validationInput.addEventListener("blur", testValidation);
+refs.validationInput.addEventListener("focus", resetValidation);
