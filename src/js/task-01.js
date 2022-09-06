640eb3bee0c6523425d073f9bc6741db2a8954df
VitaliiPoliatykin <102904854+VitaliiPoliatykin@users.noreply.github.com>
2022-09-06 21:53:16 +0300

    Прибрано зайве в ДЗ

--- src/js/task-01.js
+++
@@ -1,7 +0,0 @@
-const catElement = document.querySelectorAll(`.item`);
-console.log(`Number of categories:`, catElement.length);
-
-catElement.forEach(function (categories) {
-    console.log(`Category:`, categories.firstElementChild.textContent);
-    console.log(`Elements:`, categories.lastElementChild.children.length);
-})
--- src/js/task-07.js
+++
@@ -1,8 +0,0 @@
-const input = document.getElementById(`font-size-control`);
-const span = document.getElementById(`text`);
-
-input.addEventListener(`input`, onBlur);
-
-function onBlur(event) {
-    span.style.fontSize = event.currentTarget.value + `px`;
-}
